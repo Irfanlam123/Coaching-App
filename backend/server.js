0a858541203848eff0ec993a5298642157b1751a
Irfanlam123 <irfanalam010202@gmail.com>
2025-09-27 14:13:00 +0530

    ui

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
+// Routes
 const studyMaterialRoutes = require("./routes/studyRoute");
+const timeTableRoutes = require("./routes//timeTableRoutes"); // ✅ add this
+
 const { cleanupExpiredMaterials } = require("./controllers/studyController");
 
 const app = express();
 connectDB();
 
+// Middleware
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+// Routes
 app.use("/api/materials", studyMaterialRoutes);
+app.use("/api/notifications", timeTableRoutes); // ✅ add this
+
 app.get("/", (req, res) => res.send("✅ API is running..."));
 
 // Cleanup expired materials every hour
